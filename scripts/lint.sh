75e2bad9d062ccdefb8ed0b0e5069701d22d845f
Felix Altenberger <felix@zenml.io>
2023-02-10 10:03:50 +0100

    Add format/lint scripts

---
+++ scripts/lint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+set -x
+set -o pipefail
+
+SRC=${1:-"src"}
+
+export ZENML_DEBUG=1
+export ZENML_ANALYTICS_OPT_IN=false
+ruff $SRC
+
+# autoflake replacement: checks for unused imports and variables
+ruff $SRC --select F401,F841 --exclude "__init__.py" --isolated
+
+black $SRC  --check
+
+# check type annotations
+mypy $SRC_NO_TESTS
