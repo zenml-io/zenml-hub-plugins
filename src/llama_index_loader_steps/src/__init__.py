be96fd128ede8d2b724e1a8eaf24a46aafb90987
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-04-12 14:23:53 +0200

    make imports relative

--- src/llama_index_loader_steps/src/__init__.py
+++ src/llama_index_loader_steps/src/__init__.py
@@ -12,7 +12,7 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from llama_index_loader_step import (
+from .llama_index_loader_step import (
     llama_index_loader_step,
     LlamaIndexLoaderParameters,
 )
